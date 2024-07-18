953181b1c25cb259f30b7772d0e323a998f27fc9
Tran Bao Son - P.CN1 <sontb@efyvn.vn>
2024-07-18 10:28:12 +0700

    fix

--- src/app/qr-scan/qr-scan.component.ts
+++ src/app/qr-scan/qr-scan.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { NgxScannerQrcodeModule, ScannerQRCodeConfig, ScannerQRCodeResult } from 'ngx-scanner-qrcode';
 
 @Component({
   selector: 'app-qr-scan',
   standalone: true,
-  imports: [ZXingScannerModule],
+  imports: [NgxScannerQrcodeModule],
   templateUrl: './qr-scan.component.html',
   styleUrl: './qr-scan.component.css'
 })
@@ -12,6 +12,25 @@ export class QrScanComponent {
   currentDevice: any;
   hasPermission: boolean;
   qrResult: any
+  config: ScannerQRCodeConfig = {
+    constraints: {
+      video: {
+        width: window.innerWidth
+      },
+    },
+    // canvasStyles: [
+    //   { /* layer */
+    //     lineWidth: 1,
+    //     fillStyle: '#00950685',
+    //     strokeStyle: '#00950685',
+    //   },
+    //   { /* text */
+    //     font: '17px serif',
+    //     fillStyle: '#ff0000',
+    //     strokeStyle: '#ff0000',
+    //   }
+    // ],
+  };
   constructor() {
     this.currentDevice = null
     this.hasPermission = false
@@ -27,7 +46,14 @@ export class QrScanComponent {
 
   //Permission for the app to use the device camera
   onHasPermission(has: boolean): void {
+    0
     this.hasPermission = has;
+  }
+
+  onEvent(e: ScannerQRCodeResult[], action?: any): void {
+    // e && action && action.pause();
+    console.log(e);
+    this.qrResult = JSON.stringify(e)
   }
 
   checkQRJSON(qrString: string): boolean {
