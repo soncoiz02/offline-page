df7754898b65b8290389859e3a2dedaee0e70632
Tran Bao Son - P.CN1 <sontb@efyvn.vn>
2024-09-10 11:25:37 +0700

    test

--- src/app/share-content/share-content.component.ts
+++ src/app/share-content/share-content.component.ts
@@ -8,15 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './share-content.component.css'
 })
 export class ShareContentComponent {
-  handleShareContent(e: MouseEvent) {
+  handleShareContent(e: MouseEvent, type: string) {
     if (!!navigator.share) {
-      navigator.share({
-        title: 'Tiêu đề test',
-        text: 'Nội dung chia sẻ',
-        url: "https://devecontractapp.intrustdss.xyz/api/download-file?id=B724BE54-FE99-465A-9363-7CD3B25DBE46&view=1#toolbar=0",
-      })
-        .then(() => console.log("ok"))
-        .catch((error) => console.error('Lỗi khi chia sẻ:', error));
+      if (type === 'file') {
+        const file = new File(['Hello!'], 'test.txt', {
+          type: 'text/plain'
+        })
+        navigator.share({
+          title: 'Tiêu đề test',
+          text: 'Nội dung chia sẻ',
+          files: [file],
+        })
+          .then(() => console.log("ok"))
+          .catch((error) => console.error('Lỗi khi chia sẻ:', error));
+      }
+      else {
+        navigator.share({
+          title: 'Tiêu đề test',
+          text: 'Nội dung chia sẻ',
+          url: "https://devecontractapp.intrustdss.xyz/api/download-file?id=B724BE54-FE99-465A-9363-7CD3B25DBE46&view=1#toolbar=0",
+        })
+          .then(() => console.log("ok"))
+          .catch((error) => console.error('Lỗi khi chia sẻ:', error));
+      }
     } else {
       alert("Trình duyệt không hỗ trợ")
     }
